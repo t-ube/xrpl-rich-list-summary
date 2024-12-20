221c42abc623c8b19cc929132dec723ddeff1987
t-ube <ube@coder.okinawa.jp>
2024-12-20 15:16:12 +0900

    update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://https://xrp-rich-list-summary.shirome.net',
+      url: 'https://xrp-rich-list-summary.shirome.net',
       lastModified: new Date(),
       changeFrequency: 'hourly',
       priority: 1,
